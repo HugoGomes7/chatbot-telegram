608645f41940f9f81deb3a152fca25c158b7d5f2
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-01 09:14:09 -0300

    Middleware pattern

---
+++ middleware-pattern/middleware.js
@@ -0,0 +1,23 @@
+const execution = (context, ...middlewares) => {
+  const run = index => {
+    middlewares && index < middlewares.length &&
+      middlewares[index](context, () => run(index + 1))
+  }
+  run(0)
+}
+
+const middleware1 = (context, next) => {
+  context.info1 = 'middleware1'
+  next()
+}
+
+const middleware2 = (context, next) => {
+  context.info2 = 'middleware2'
+  next()
+}
+
+const middleware3 = context => context.info3 = 'middleware3'
+
+const context = {}
+execution(context, middleware1, middleware2, middleware3)
+console.log(context)
