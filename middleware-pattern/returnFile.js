accf7ce01c7490274feed31c1aa39aa54ee10a02
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-05 00:29:08 -0300

    Getting photo file with Axios

--- middleware-pattern/returnFile.js
+++ middleware-pattern/returnFile.js
@@ -3,8 +3,16 @@ const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
 const axios = require('axios')
 
-but.on('voice', async context => {
+bot.on('voice', async context => {
   const id = context.update.message.voice.file_id
   const reply = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
   context.replyWithVoice({ url: `${env.apiFileUrl}/${reply.data.result.file_path}` })
 })
+
+bot.on('photo', async context => {
+  const id = context.update.message.photo[0].file_id
+  const reply = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
+  context.replyWithPhoto({ url: `${env.apiFileUrl}/${reply.data.result.file_path}` })
+})
+
+bot.startPolling()
