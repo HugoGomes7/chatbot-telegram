7069c44e12d58f304b737295344090a072ab6921
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-05 00:22:11 -0300

    Getting voice file with Axios

--- middleware-pattern/returnFile.js
+++ middleware-pattern/returnFile.js
@@ -2,3 +2,9 @@ const env = require('../.env')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
 const axios = require('axios')
+
+but.on('voice', async context => {
+  const id = context.update.message.voice.file_id
+  const reply = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
+  context.replyWithVoice({ url: `${env.apiFileUrl}/${reply.data.result.file_path}` })
+})
