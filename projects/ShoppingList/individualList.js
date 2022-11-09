114e0d49863dd0345afd616e7d9725e100b5179c
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-09 11:07:10 -0300

    UserId verification

--- projects/ShoppingList/individualList.js
+++ projects/ShoppingList/individualList.js
@@ -14,20 +14,32 @@ const functionButtons = list => Extra.markup(
 
 bot.use(session())
 
-bot.start(async context => {
+const verifyUser = (context, next) => {
+  const sameIdMessage = context.update.message && context.update.message.from.id == env.userId
+  const sameIdCallback = context.update.callback_query && context.update.callback_query.from.id == env.userId
+
+  if (sameIdMessage || sameIdCallback) {
+    next()
+  }
+  else {
+    context.reply('Sorry, I am not allowed to talk to you')
+  }
+}
+
+bot.start(verifyUser, async context => {
   const name = context.update.message.from.first_name
   await context.reply(`Welcome, ${name}!`)
   await context.reply('Write the items you want to add...')
   context.session.list = []
 })
 
-bot.on('text', context => {
+bot.on('text', verifyUser, context => {
   let msg = context.update.message.text
   context.session.list.push(msg)
   context.reply(`${msg} added!`, functionButtons(context.session.list))
 })
 
-bot.action(/delete (.+)/, context => {
+bot.action(/delete (.+)/, verifyUser, context => {
   context.session.list = context.session.list.filter(
     item => item !== context.match[1])
   context.reply(`${context.match[1]} deleted!`, functionButtons(context.session.list))
