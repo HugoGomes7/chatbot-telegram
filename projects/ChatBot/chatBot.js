dce9c6e2008405eb91fe5bfab1a901f504be230e
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-13 22:42:28 -0300

    Replying keyboardOptions using bot.hears()

--- projects/ChatBot/chatBot.js
+++ projects/ChatBot/chatBot.js
@@ -6,9 +6,10 @@ const axios = require('axios')
 const bot = new Telegraf(env.token)
 
 const keyboardOptions = Markup.keyboard([
-  ['Whats are bots?', 'Who created me?'],
+  ['What are bots?', 'Who created me?'],
   ['Can I automate tasks?'],
-  ['How I was developed?']
+  ['How I was developed?'],
+  ['What was I developed for?']
 ]).resize().extra()
 
 bot.start(async context => {
@@ -17,5 +18,33 @@ bot.start(async context => {
   await context.replyWithPhoto('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4C7g7qNmxye01hIWsnHgfNDZ8NLbXMB_6pSdFJufx&s')
   await context.replyWithMarkdown(`_Can I help you with anything?_`, keyboardOptions)
 })
+
+bot.hears('What are bots?', context => {
+  context.replyWithMarkdown('Check out this article I found on google :) [link](https://www.kaspersky.com.br/resource-center/definitions/what-are-bots)', keyboardOptions)
+})
+
+bot.hears('Who created me?', async context => {
+  await context.replyWithMarkdown('Who developed me is a very talented and cool guy, it is worth checking out his networks:')
+  await context.replyWithMarkdown('LinkedIn: [link](https://www.linkedin.com/in/hugo-gomes-889658211)')
+  await context.replyWithMarkdown('Github: [link](https://github.com/HugoGomes7)')
+  context
+  await context.replyWithMarkdown(`\n\n_Can I help you with anything?_`, keyboardOptions)
+})
+
+bot.hears('How I was developed?', async context => {
+  await context.replyWithMarkdown('I was developed with Javascript and NodeJS.')
+  await context.replyWithMarkdown('You can see all development in the project repository on Github: [link](https://github.com/HugoGomes7/chatbot-telegram)')
+  await context.replyWithMarkdown(`\n\n_Can I help you with anything?_`, keyboardOptions)
+})
+
+bot.hears('What was I developed for?', async context => {
+  await context.replyWithMarkdown('To be integrated into two other projects:')
+  await context.replyWithMarkdown('1. Shopping List')
+  await context.replyWithMarkdown('2. Schedule')
+  await context.replyWithMarkdown('You can see all development in the project repository on Github: [link](https://github.com/HugoGomes7/chatbot-telegram)')
+  await context.replyWithMarkdown(`\n\n_Can I help you with anything?_`, keyboardOptions)
+})
+
+
 
 bot.startPolling()
